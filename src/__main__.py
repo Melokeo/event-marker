ea96132f28ef058579d846305682d751e1517194
Melokeo <penkonno@gmail.com>
2025-11-11 11:27:28 -0500

    add entry command

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,12 @@ from PyQt6.QtWidgets import QApplication
 
 from gui import VideoPlayer
 
-if __name__ == '__main__':
+def main():
+    """Main entry point for the event-marker application."""
     app = QApplication(sys.argv)
     player = VideoPlayer(app=app)
     player.show()
     sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
